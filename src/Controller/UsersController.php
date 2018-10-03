ad430dca3424711e83e1460a3c8fff307d38d81e
Gnana Priya <gnana.priya@zoomrx.com>
2018-10-03 15:15:50 +0530

    Added Users to resources

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -38,10 +38,9 @@ class UsersController extends AppController
      */
     public function view($id = null)
     {
-        $user = $this->Users->get($id, [
-            'contain' => ['Levels', 'Teams', 'Boards']
-        ]);
+        $user = $this->Users->get($id);
 
+        $this->set('_serialize', true);
         $this->set('user', $user);
     }
 
